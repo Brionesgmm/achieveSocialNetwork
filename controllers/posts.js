d8edf4e925ad956010f78ff1a38470e2dcdc362d
Brionesgmm <99777937+Brionesgmm@users.noreply.github.com>
2023-03-05 22:02:24 -0700

    added comments and usernames

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,9 +1,12 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
+const Comment = require("../models/Comment");
+
 
 module.exports = {
   getProfile: async (req, res) => {
     try {
+      console.log(res)
       const posts = await Post.find({ user: req.user.id });
       res.render("profile.ejs", { posts: posts, user: req.user });
     } catch (err) {
@@ -20,8 +23,10 @@ module.exports = {
   },
   getPost: async (req, res) => {
     try {
+      console.log(req.user)
       const post = await Post.findById(req.params.id);
-      res.render("post.ejs", { post: post, user: req.user });
+      const comments = await Comment.find({post: req.params.id}).sort({ createdAt: "desc" }).lean();
+      res.render("post.ejs", { post: post, user: req.user, comments: comments });
     } catch (err) {
       console.log(err);
     }
